7e4c47beefb4347696950cc4bdbd637c54dea14b
minseon <ellastin1023@naver.com>
2025-11-13 17:49:52 +0900

    commit

---
+++ core/core/src/main/java/hello/core/member/MemberServiceImpl.java
@@ -0,0 +1,18 @@
+package hello.core.member;
+
+import hello.core.member.MemoryMemberRepository;
+
+public class MemberServiceImpl implements MemberService {
+
+    private final MemberRepository memberRepository = new MemoryMemberRepository();
+
+    @Override
+    public void join(Member member) {
+        memberRepository.save(member);
+    }
+
+    @Override
+    public Member findMember(Long memberId) {
+        return memberRepository.findById(memberId);
+    }
+}
